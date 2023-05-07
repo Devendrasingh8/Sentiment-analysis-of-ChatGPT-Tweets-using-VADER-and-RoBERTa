e42e9ce084a89a29910bff6b1fe3790acff0c259
Devendrasingh Jhala <devendrasingh.jhala1996@gmail.com>
2023-05-07 02:58:45 -0400

    Add files via upload

---
+++ Roberta_Sentiment.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import numpy
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
+from tqdm import tqdm
+import json
+import sys 
+
+# Sentiment Analysis using Roberta Pretrained Model
+
+
+def Roberta(df):
+    MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    
+   
+    Scores_df= pd.DataFrame(columns= ('R_neg','R_neu', 'R_pos','max'))
+    
+    i=0
+    for row in tqdm(df['tweets'],desc=" RoBERTa Sentiment Analysis", total= len(df),  position=0, leave=True):
+        
+        encoded_text = tokenizer(row, return_tensors='pt')
+        output = model(**encoded_text)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+        
+        Scores_df.loc[i] = (scores[0], scores[1], scores[2], max(scores[0], scores[1], scores[2]) )
+        i +=1
+        
+    
+    df=pd.concat([df.reset_index(drop=True),Scores_df],axis=1)
+    
+ 
+    
+    return(df)
+  
